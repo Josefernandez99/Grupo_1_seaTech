320063b1db57c3bebf2b87e91f1315d0e8a54ac6
Francisco Polzoni <francisco.polzoni@gpsde.local>
2023-11-08 15:12:13 -0300

    Se modificaron nombres de archivos y se agregó productDetail .htm y .css

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const puerto = 3000
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
+app.get('/productDetail',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'))
+});
+
+const puerto = 3000
 app.listen(puerto, () => 
 console.log('Servidor corriendo puerto:' + puerto));
-
-app.get(express.static(path.resolve(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/index.html'))
-})
