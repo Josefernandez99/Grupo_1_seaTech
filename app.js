d1d2033c1ef28dec06feb72047a25948e82d0b25
VS1379 <valentin.silva.user@gmail.com>
2023-11-08 21:14:28 -0300

    update css and views

--- app.js
+++ app.js
@@ -10,10 +10,26 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
+app.get('/index',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
 app.get('/productDetail',(req,res) => {
     res.sendFile(path.join(__dirname, 'views/productDetail.html'))
 });
 
+app.get('/login',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+});
+
+app.get('/cart',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/cart.html'))
+});
+
+app.get('/register',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
+});
+
 const puerto = 3000
 app.listen(puerto, () => 
-console.log('Servidor corriendo puerto:' + puerto));
+console.log('Servidor corriendo puerto: http://localhost:' + puerto));
