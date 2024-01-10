dabd536a9ef22c941e5e7851698a2933e8914647
GuilleGonz8 <guillermogonzalez.edu@gmail.com>
2024-01-10 04:26:30 -0300

    models delete

--- models/Productos.js
+++ models/Productos.js
@@ -85,8 +85,11 @@ const Producto = {
         return true;
 
     },
-    delete: function () {
-
+    delete: function (id) {
+        let allProducts = this.findAll();
+        let finaProducts = allProducts.filter(oneProduct => oneProduct.id !== id);
+        fs.writeFileSync(path.join(__dirname, this.filename), JSON.stringify(finalProducts, null, ' '));
+        return true;
     }
 
 
