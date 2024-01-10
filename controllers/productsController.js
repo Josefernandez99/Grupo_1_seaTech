067627767344180d8d2154ba7f5ea418c586dda6
VS1379 <valentin.silva.user@gmail.com>
2024-01-10 05:41:03 -0300

    delete and validations working

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,42 +1,41 @@
 const { validationResult } = require("express-validator");
-const product = require('../models/Productos')
+const product = require("../models/Productos");
 
 const controller = {
-    cart: function (req, res) {
-        res.render('./products/cart');
-    },
-    detail: function (req, res) {
-        res.render('./products/productDetail');
-    },
-    add: function (req, res) {
-        res.render('./products/productAdd');
-    },
-    create: function (req, res) {
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            product.create(req.body);
-            //Sujeto a cambios
-            return res.redirect("/");
-        }
-        let oldBody = req.body;
-        errors = errors.mapped();
-        console.log(errors);
-        res.render("./products/productAdd", { errors, oldBody });
-    },
-    edit: function (req, res) {
-
-        res.render('./products/productEdit');
-    },
-    list: function (req, res) {
-        const allProducts = product.findAll();
-        res.render('./products/listaProductos', { allProducts });
-    },
-    delete: function (req, res) {
-        res.render("./products/deleteSure");
-    },
-    deleteSure: function (req, res) {
-        product.delete(req.params.id);
-    },
-}
+  cart: function (req, res) {
+    res.render("./products/cart");
+  },
+  detail: function (req, res) {
+    res.render("./products/productDetail");
+  },
+  add: function (req, res) {
+    res.render("./products/productAdd");
+  },
+  create: function (req, res) {
+    let errors = validationResult(req);
+    console.log(errors);
+    if (errors.isEmpty()) {
+      product.create(req.body);
+      //Sujeto a cambios
+      return res.redirect("/");
+    }
+    let oldBody = req.body;
+    errors.mapped();
+    res.render("./products/productAdd", { errors, oldBody });
+  },
+  edit: function (req, res) {
+    res.render("./products/productEdit");
+  },
+  list: function (req, res) {
+    const allProducts = product.findAll();
+    res.render("./products/listaProductos", { allProducts });
+  },
+  delete: function (req, res) {
+    res.render("./products/deleteSure");
+  },
+  deleteSure: function (req, res) {
+    product.delete(req.params.id);
+  },
+};
 
 module.exports = controller;
--- routes/products.Routes.js
+++ routes/products.Routes.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const productsController = require('../controllers/productsController');
+const productsController = require("../controllers/productsController");
+const productValidate = require("../middlewares/productValidate");
 
 //Listado de productos
-route.get('/', productsController.list);
+route.get("/", productsController.list);
 
 //Vista del carrtito
-route.get('/cart', productsController.cart);
+route.get("/cart", productsController.cart);
 
 //Vista de crear un producto
-route.get('/create', productsController.add);
+route.get("/create", productsController.add);
 
 //Procesar la creación de un producto
-route.post('/create', productsController.create);
+route.post("/create", productValidate, productsController.create);
 
 //Vista de un producto en particular
-route.get('/:id', productsController.detail);
+route.get("/:id", productsController.detail);
 
 //Vista de editar un producto en particular
-route.get('/detail/:id/edit', productsController.edit);
+route.get("/detail/:id/edit", productsController.edit);
 
 //Procesar la edición de un producto en particular
 //route.put('/:id/edit', productsController.update);
