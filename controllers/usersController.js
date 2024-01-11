8fb99b27966c6ef4b0cc7c81a6f18cc12b359322
VS1379 <valentin.silva.user@gmail.com>
2024-01-11 00:44:04 -0300

    add Usuarios Model

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,14 @@
+
 const controller = {
-    login: function (req, res) {
-        res.render('./users/login');
-    },
-    register: function (req, res) {
-        res.render('./users/register');
-    }
-}
+  login: function (req, res) {
+    res.render("./users/login");
+  },
+  checkLogin: function (req, res) {
+    res.render("./users/login");
+  },
+  register: function (req, res) {
+    res.render("./users/register");
+  },
+};
 
 module.exports = controller;
