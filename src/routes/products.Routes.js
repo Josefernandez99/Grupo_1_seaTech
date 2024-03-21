b6f0f0145f25b7234b88d23f73ff88a02b952678
NahuelNour <lucasnahuelnour2016@gmail.com>
2024-03-21 00:41:28 -0300

    newStyle

--- src/routes/products.Routes.js
+++ src/routes/products.Routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const route = express.Router();
 const productsController = require("../controllers/productsController");
-const productValidate = require("../middlewares/productValidate");
+/*const productValidate = require("../middlewares/productValidate");
 const upload = require('../middlewares/multer.Middleware');
 const { cloudinaryMiddleware } = require("../middlewares/cloudinary.Middleware");
-const authMiddleware = require("../middlewares/authMiddlewareRoute");
+const authMiddleware = require("../middlewares/authMiddlewareRoute"); */
 
 //Listado de productos
 route.get("/", productsController.list);
@@ -14,22 +14,22 @@ route.get("/cart", productsController.cart);
 route.post("/cart", productsController.cartAdd);
 
 //Vista de crear un producto
-route.get("/add", authMiddleware, productsController.add);
+route.get("/add", productsController.add);
 
 //Procesar la creación de un producto
-route.post("/create", upload.single('image'), productValidate, cloudinaryMiddleware, productsController.create);
+route.post("/create", productsController.create);
 
 //Vista de un producto en particular
 route.get("/detail/:id", productsController.detail);
 
 //Vista de editar un producto en particular
-route.get("/detail/:id/edit", authMiddleware, productsController.edit);
+route.get("/detail/:id/edit", productsController.edit);
 
 //Procesar la edición de un producto en particular
-route.put('/detail/:id/update', upload.single('image'), productValidate, cloudinaryMiddleware, productsController.update);
+route.put('/detail/:id/update', productsController.update);
 
 //Eliminar producto
-route.get("/detail/:id/delete", authMiddleware, productsController.delete);
+route.get("/detail/:id/delete", productsController.delete);
 route.delete("/detail/:id/destroy", productsController.destroy);
 
 module.exports = route;
--- src/routes/users.Routes.js
+++ src/routes/users.Routes.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const route = express.Router();
 const usersController = require("../controllers/usersController");
-const upload = require('../middlewares/multer.Middleware');
+/*const upload = require('../middlewares/multer.Middleware');
 const userValidate = require("../middlewares/userValidate.Middleware");
 const { cloudinaryMiddleware } = require("../middlewares/cloudinary.Middleware");
 const guesMiddleware = require("../middlewares/guestMiddlewareRoute");
-const authMiddleware = require("../middlewares/authMiddlewareRoute");
+const authMiddleware = require("../middlewares/authMiddlewareRoute"); */
 
-route.get("/login", guesMiddleware, usersController.login);
-route.post("/login", userValidate, usersController.checkLogin);
+route.get("/login",  usersController.login);
+route.post("/login",  usersController.checkLogin);
 
-route.get("/register", guesMiddleware, usersController.register);
-route.post("/register", upload.single('image'), userValidate, cloudinaryMiddleware, usersController.registerUser);
+route.get("/register", usersController.register);
+route.post("/register",  usersController.registerUser);
 
-route.get("/logout", authMiddleware, usersController.logout);
+route.get("/logout", usersController.logout);
 
-route.get("/profile/:id", authMiddleware, usersController.profile);
+route.get("/profile/:id", usersController.profile);
 
 module.exports = route;
